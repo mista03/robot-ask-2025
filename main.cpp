74acf988c64eedb63f9346fc0e384ba4003f67df
Michał Stachowiak <michal.stachowiak@student.put.poznan.pl>
2025-05-10 18:16:47 +0200

    ver3

--- main.cpp
+++ main.cpp
@@ -1,36 +1,41 @@
 #include <AFMotor.h>
-#include <NewPing.h>
 #include <Servo.h>
 
 #define TRIG_PIN 2
 #define ECHO_PIN 13
 #define SERVO_PIN 10
+
 #define LEFT 0
 #define BACK 1
 #define RIGHT 2
+#define NONE 3
 
-uint8_t maxDist = 300;
-uint8_t stopDist = 40;
-uint8_t pauseDist = 30;
-// timeout = 2 * (maxDist + 10) / 100 / 340 * 1000000 = (maxDist + 10) * 58.82
-uint16_t timeout = (maxDist + 10) * 59;
-
-uint8_t motorSpeed = 80;
-uint8_t launchSpeed = 120;
-uint8_t turnSpeed = 160;
-uint16_t turnTime = 600;
-
-uint8_t offsetRL = 18;
-uint8_t offsetFL = 18;
-uint8_t offsetRR = 0;
-uint8_t offsetFR = 0;
+uint8_t getDirection(uint8_t priority = NONE);
 
 AF_DCMotor RR(1);
 AF_DCMotor FR(2);
 AF_DCMotor FL(3);
 AF_DCMotor RL(4);
 Servo servo;
-NewPing sonar(TRIG_PIN, ECHO_PIN, maxDist);
+
+uint8_t lastServoRotation;
+
+uint8_t maxDist = 200;
+uint8_t stopDist = 40;
+uint8_t sideDist = 30;
+// timeout = 2 * (maxDist + 10) / 100 / 340 * 1000000 = (maxDist + 10) * 58.82
+uint16_t timeout = (maxDist + 10) * 59;
+
+uint8_t motorSpeed = 80;
+uint8_t slowSpeed = 60;
+uint8_t launchSpeed = 120;
+uint8_t turnSpeed = 160;
+uint16_t turnTime = 600;
+
+uint8_t offsetRL = 20;
+uint8_t offsetFL = 20;
+uint8_t offsetRR = 0;
+uint8_t offsetFR = 0;
 
 /*
 TODO:
@@ -55,42 +60,49 @@ void setup() {
 void loop() {
   servo.write(90);
   delay(500);
-  uint16_t distance = sonar.ping_cm();
+  lastServoRotation = 90;
+
   uint8_t counter = 0;
+  uint8_t priority = NONE;
+  uint16_t distance = getDistance();
 
   if (distance >= stopDist)
-    accelerate();
+    changeSpeed(launchSpeed, motorSpeed);
 
   while (distance >= stopDist) {
-    if (counter >= 4) {
-      servo.write(130);
-      delay(200);
-      distance = sonar.ping_cm();
-      if (distance < pauseDist) break;
+    if (counter >= 5) {
+      changeSpeed(motorSpeed, slowSpeed);
 
-      servo.write(90);
-      delay(200);
-      distance = sonar.ping_cm();
-      if (distance < pauseDist) break;
+      if (checkSide(130)) {
+        priority = RIGHT;
+        break;
+      }
+      // if (checkSide(90, 250)) {
+      //   priority = LEFT;
+      //   break;
+      //}
+      if (checkSide(50)) {
+        priority = LEFT;
+        break;
+      }
 
-      servo.write(50);
-      delay(200);
-      distance = sonar.ping_cm();
-      if (distance < pauseDist) break;
-
-      servo.write(90);
       counter = 0;
+      servo.write(90);
+      lastServoRotation = 90;
+
+      changeSpeed(slowSpeed, motorSpeed);
+    } else {
+      delay(200);
     }
       
-    delay(200);
-
-    distance = sonar.ping_cm();
+    distance = getDistance();
     counter++;
   }
 
   stop();
 
-  uint8_t turnDir = getDirection();
+  uint8_t turnDir = getDirection(priority);
+  Serial.print("turn = ");
   Serial.println(turnDir);
 
   switch (turnDir) {
@@ -106,25 +118,22 @@ void loop() {
   }
 }
 
-void accelerate() {
-  RL.setSpeed(launchSpeed + offsetRL);
-  FL.setSpeed(launchSpeed + offsetFL);
-  RR.setSpeed(launchSpeed + offsetRR);
-  FR.setSpeed(launchSpeed + offsetFR);
-  
-  RL.run(FORWARD);
-  FL.run(FORWARD);
-  RR.run(FORWARD);
-  FR.run(FORWARD);
+void changeSpeed(uint8_t initial, uint8_t target) {
+  if (initial == target) return;
+  uint8_t step = (target > initial) ? 1 : -1;
 
-  int i = launchSpeed - 1;
-  while (i > motorSpeed) {
+  for (uint8_t i = initial; i != target; i += step) {
     RL.setSpeed(i + offsetRL);
     FL.setSpeed(i + offsetFL);
     RR.setSpeed(i + offsetRR);
     FR.setSpeed(i + offsetFR);
+
+    RL.run(FORWARD);
+    FL.run(FORWARD);
+    RR.run(FORWARD);
+    FR.run(FORWARD);
+
     delay(10);
-    i--;
   }
 }
 
@@ -164,44 +173,61 @@ void turn(uint8_t direction, uint16_t duration) {
   FR.run(RELEASE);
 }
 
-// uint16_t getDistance() {
-//   uint32_t pingTime;
-//   uint16_t distance;
+bool checkSide(uint8_t degrees) {
+  uint8_t degreeChange = abs(lastServoRotation - degrees);
+  lastServoRotation = degrees;
 
-//   digitalWrite(TRIG_PIN, HIGH);
-//   delayMicroseconds(10);
-//   digitalWrite(TRIG_PIN, LOW);
+  servo.write(degrees);
+  delay(degreeChange * 6);
+  uint16_t distance = getDistance();
 
-//   pingTime = pulseIn(ECHO_PIN, HIGH);
+  return distance < sideDist;
+}
+
+uint16_t getDistance() {
+  uint32_t pingTime;
+  uint16_t distance;
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  pingTime = pulseIn(ECHO_PIN, HIGH, timeout);
   
-//   // distance = pingTime * 340 * 100 / 1000000 / 2 = pingTime * 0.017
-//   distance = pingTime * 0.017;
-//   Serial.println(distance);
+  // distance = pingTime * 340 * 100 / 1000000 / 2 = pingTime * 0.017
+  distance = pingTime * 0.017;
+  Serial.print("dist = ");
+  Serial.println(distance);
   
-//   return distance;
-// }
+  return distance;
+}
 
-uint8_t getDirection() {
-  uint16_t distanceL, distanceR;
+uint8_t getDirection(uint8_t priority = NONE) {
+  uint16_t distances[2] = {0, 0};
   uint8_t turnDir;
 
-  servo.write(180);
-  delay(500);
-  distanceL = sonar.ping_cm();
+  if (priority == LEFT || priority == NONE) {
+    servo.write(180);
+    delay(abs(lastServoRotation - 180) * 6);
+    lastServoRotation = 180;
+    distances[0] = getDistance();
+  }
   
-  servo.write(0);
-  delay(1000);
-  distanceR = sonar.ping_cm();
+  if (priority == RIGHT || priority == NONE) {
+    servo.write(0);
+    delay(lastServoRotation * 6);
+    lastServoRotation = 180;
+    distances[1] = getDistance();
+  }
 
-  if (distanceL >= maxDist && distanceR >= maxDist)
+  if (distances[0] >= maxDist && distances[1] >= maxDist)
     turnDir = LEFT;
-  else if (distanceL <= stopDist && distanceR <= stopDist)
+  else if (distances[0] <= stopDist && distances[1] <= stopDist)
     turnDir = BACK;
-  else if (distanceL >= distanceR)
+  else if (distances[0] >= distances[1])
     turnDir = LEFT;
-  else
+  else if (distances[0] < distances[1])
     turnDir = RIGHT;
 
   return turnDir;
 }
-
